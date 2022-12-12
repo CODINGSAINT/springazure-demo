38ef6293a4adda1c56ad2baafc2bb657f507ade7
Pallav <07.pallav@gmail.com>
2022-12-12 21:56:21 +0530

    Spring Azure Demo Cloud App

---
+++ src/main/java/com/codingsaint/springazuredemo/SpringazureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codingsaint.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringazureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringazureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingsaint/springazuredemo/SpringazureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingsaint.springazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringazureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
